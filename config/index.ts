f5f16d2880e7d33c29fadb5237a74bb38c60de7f
Emdadul Islam <emdadulislam580@gmail.com>
2024-12-05 16:42:42 +0600

    feat(announcements): add "Announcements" menu item for teacher navigation

--- config/index.ts
+++ config/index.ts
@@ -220,6 +220,13 @@ export function getTeacherMenuList(pathname: string): Group[] {
           icon: HomeIcon,
           submenus: [],
         },
+        {
+          href: "/teacher/announcement",
+          label: "Announcements",
+          active: ["/teacher/announcement"].includes(pathname),
+          icon: MessageSquare,
+          submenus: [],
+        },
         // {
         //   href: "/teacher/course-details/1",
         //   label: "Courses",
